967d062e459d0b487a4286193cbcd957040ae28a
Gulsum Gudukbay <gudukbay@gmail.com>
2017-12-15 17:12:37 +0300

    meanshift done, not tested

--- main.m
+++ main.m
@@ -4,12 +4,9 @@ clear;
 
 
 img = imread('cs484_hw3_data/15088.jpg');
-figure;
-imshow(img);
+stopThresh = 0.00075;
+winSize = 20;
+params = [winSize stopThresh];
+[labels, peaks] = segmentImage(img, params);
 
-xyz = rgb2xyz(img);
-luv_cform = makecform('xyz2uvl');
-luv_img = applycform(xyz, luv_cform);
-figure;
-imshow(luv_img);
 
